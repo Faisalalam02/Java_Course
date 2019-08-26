9b0a38e7e350640db9267c85bfc8787a27938aff
faisalalam02 <m.faisalalam12@gmail.com>
2019-08-26 15:00:07 +0500

    Chapter 5 commit

--- src/chapter5/PhoneBillCalculator.java
+++ src/chapter5/PhoneBillCalculator.java
@@ -4,21 +4,48 @@ import java.util.Scanner;
 
 public class PhoneBillCalculator {
 
+    static double overminuteRate = .25;
+    static double taxRate = .15;
+
     public static void main (String args[])
     {
-        double overminuteRate = .25;
-        double taxRate = .15;
-
         Scanner scanner  = new Scanner(System.in);
-        System.out.println("Please enter Plan Fee. ");
+        System.out.println("Please enter base cost of the plan: ");
         double planFee = scanner.nextDouble();
         System.out.println("Please enter number of overage minutes");
         double overageMinutes = scanner.nextDouble();
 
+        // calling  overagefeeCalculation
+        double overageTotal = overagefeeCalculation(overageMinutes);
+
+        // calling tax calculation method
+        double calculatedTax = taxCalculation(planFee, overageTotal);
+
+        // calling totalBill method
+        double totalBill = totalBill(planFee, overageTotal, calculatedTax);
+        scanner.close();
+
+        System.out.println("Phone bill statement.");
+        System.out.println("Plan: $" + planFee);
+        System.out.println("Overage: $" + overageMinutes);
+        System.out.println(String.format("Tax: $%.2f" , calculatedTax));
+        System.out.println(String.format("Total Bill: $%.2f" , totalBill));
     }
 
-    public static void taxCalculation ()
+    public static double overagefeeCalculation(double overageMinutes1)
     {
+        double overageTotal = overageMinutes1 * overminuteRate;
+        return overageTotal;
+    }
 
+    public static double taxCalculation (double planFee1 , double overageTotal1)
+    {
+           double totalTax =  (planFee1 + overageTotal1) * taxRate;
+           return totalTax;
+    }
+    public static double totalBill (double planFee2, double overagetotal2 , double calculatedTax1)
+    {
+        double billtotal = planFee2 + overagetotal2 + calculatedTax1;
+        return billtotal;
     }
 }
