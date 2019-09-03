37194ae8c5808878e6ebfb32b2002e282fa5f955
faisalalam02 <m.faisalalam12@gmail.com>
2019-09-03 12:19:48 +0500

    chapter 11 initial phase

---
+++ src/chapter11/Duck.java
@@ -0,0 +1,14 @@
+package chapter11;
+
+public class Duck extends Animal {
+
+    private String sound;
+
+    @Override
+    void makeSound() {
+
+        System.out.println("Quack");
+
+    }
+
+}
---
+++ src/chapter11/Horse.java
@@ -0,0 +1,9 @@
+package chapter11;
+
+public class Horse extends Animal {
+
+    @Override
+    void makeSound() {
+        System.out.println("Horse sounds neigh");
+    }
+}
